eb71306f1400df9d44975a64708577bcc671cf34
ovus <96862158+ovusike@users.noreply.github.com>
2023-06-14 20:47:22 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,14 @@ resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
   monitoring = true
+  ebs_optimized = true
+  metadata_options {
+      http_endpoint = "enabled"
+      http_tokens   = "required"
+    }
+  root_block_device {
+      encrypted     = true
+    }
 tags = {
     Name = var.name
   }
